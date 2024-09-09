58120783a3ca03376dc2dd44349e047ff69127b7
ParagNetwork <166380945+ParagNetwork@users.noreply.github.com>
2024-09-09 11:39:08 +0530

    design

--- src/component/list.js
+++ src/component/list.js
@@ -2,14 +2,51 @@ import "../../src/index.css";
 import items from '../image/item.jpeg';
 
 const List = ({  }) => {
-
+const  images = [
+    {
+      id: 1,
+      image: items,
+    },
+    {
+      id: 2,
+      image: items,
+    },
+    {
+      id: 3,
+      image: items,
+    },
+    {
+      id: 4,
+      image: items,
+    },
+    {
+      id: 5,
+      image: items,
+    },
+    {
+      id: 6,
+      image: items,
+    },
+    {
+      id: 7,
+      image: items,
+    },
+    {
+      id: 8,
+      image: items,
+    },
+    {
+      id: 9,
+      image: items,
+    },
+]
   return (
    <>
     <div className="list-grid">
-       {[1,2,3,4,5,6,7,8,9,10].map((item) => (
+       {images.map((item) => (
         <div className="list-grid-item">
             <div>
-                <img src={items} alt="item" className="list-item-image" />
+                <img src={item.image} alt="item" className="list-item-image" />
             </div>
         </div>
          ))}
